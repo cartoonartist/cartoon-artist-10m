a6383b1b45e19205016f9be8b22097b39cb121ca
cartoonartist <cartoonartist10m@gmail.com>
2025-12-31 11:48:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -115,7 +115,7 @@ export default function App() {
     {
       id: 46 ,
       title: " luffy vs kaido",
-      price: "₹500",
+      price: "₹800",
       img: "/art46.jpg",
       sold: false,
       desc: " An intense action scene showing Monkey D. Luffy facing Kaido’s overwhelming power. Dynamic motion, bold blues, and fierce expressions reflect the clash of wills between freedom and tyranny in this epic battle."
